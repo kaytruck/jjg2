9e23479235c337b39cad75f1b7a5a1888357e8e5
kaytruck <kayta.sino@gmail.com>
2020-05-10 14:57:55 +0900

    地面の描画と、穴に落下時の判定を追加

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-# ゲーム定数
+"""
+ゲーム定数
+"""
 MOVE_X_STEP = 10    # 横方向のステップ
 SCREEN_WIDTH = 800  # 画面の横幅
 SCREEN_HEIGHT = 600 # 画面の高さ
 PLAYER_SIZE = 20    # プレイヤーキャラの大きさ
+FLOOR_HEIGHT = 70   # スタート時の地面の高さ
--- gameroutine.py
+++ gameroutine.py
@@ -5,18 +5,11 @@ import config
 from gamestatus import GameStatus
 
 class GameRoutine:
-    # ゲーム変数
-    # p_x = 0
-    # p_y = 0
-    # jump_v = 0
-    # is_jumping = False
-    # is_landing = True
 
     # ゲーム初期化処理
     def __init__(self, screen):
-        # global p_x, p_y, jump_v, is_jumping, is_landing
         self.p_x = 400
-        self.p_y = config.SCREEN_HEIGHT - config.PLAYER_SIZE
+        self.p_y = config.SCREEN_HEIGHT - config.PLAYER_SIZE - config.FLOOR_HEIGHT
         self.jump_v = 0
         self.is_jumping = False
         self.is_landing = True
@@ -26,7 +19,6 @@ class GameRoutine:
 
     # ゲーム進行処理
     def step(self, keycode):
-        # global p_x, p_y, jump_v, is_jumping, is_landing
 
         if keycode == K_LEFT:
             self.p_x -= config.MOVE_X_STEP
@@ -39,26 +31,36 @@ class GameRoutine:
         elif self.p_x >= config.SCREEN_WIDTH:
             self.p_x = config.SCREEN_WIDTH
 
+        player_char = Rect((self.p_x - 10, self.p_y), (config.PLAYER_SIZE, config.PLAYER_SIZE))
+        r1 = Rect(700, 450, 50, 100)     # TODO ジャンプ実験用ブロック
+        r2 = Rect(50, 400, 50, 100)    # TODO 終了判定実験用ブロック
+
+        # TODO 実験用の地面
+        floors = []
+        floors.append(Rect(0, config.SCREEN_HEIGHT - 20, 200, config.FLOOR_HEIGHT))
+        floors.append(Rect(300, config.SCREEN_HEIGHT - config.FLOOR_HEIGHT, 450, config.FLOOR_HEIGHT))
+
+        for f in floors:
+            if player_char.colliderect(f):
+                self.is_landing = True
+                self.p_y = f.top - config.PLAYER_SIZE
+                player_char = Rect((self.p_x - config.PLAYER_SIZE / 2, self.p_y), (config.PLAYER_SIZE, config.PLAYER_SIZE))
+            
         # 描画
         self.screen.fill((0, 0, 0))
-        player_char = Rect((self.p_x - 10, self.p_y), (config.PLAYER_SIZE, config.PLAYER_SIZE))
-        r1 = Rect(700, 550, 50, 70)
-        r2 = Rect(100, 500, 50, 100) # 終了判定実験用ブロック
-
-        if player_char.colliderect(r1):
-            self.is_landing = True
-            self.p_y = r1.top - config.PLAYER_SIZE
-            player_char = Rect((self.p_x - 10, self.p_y), (config.PLAYER_SIZE, config.PLAYER_SIZE))
-            
         pygame.draw.rect(self.screen, (200, 0, 0), player_char)
         pygame.draw.rect(self.screen, (0, 100, 200), r1)
         pygame.draw.rect(self.screen, (100, 100, 200), r2)
-        
+        for f in floors:
+            pygame.draw.rect(self.screen, (100, 200, 100), f)
+
+        # TODO お邪魔ブロックとの衝突で終了判定実験
         if player_char.colliderect(r2):
             return GameStatus.GAME_OVER
 
-        if self.p_y + config.PLAYER_SIZE >= config.SCREEN_HEIGHT:
-            self.is_landing = True
+        # 穴に落ちた判定
+        if (self.p_y + config.PLAYER_SIZE) >= config.SCREEN_HEIGHT:
+            return GameStatus.GAME_OVER
 
         # 自動的にジャンプする
         if self.is_landing:
